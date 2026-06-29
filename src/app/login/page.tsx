13ef9d71bdd9267ed6fd011725354d54265d7f87
Kendoll Castro <kendollcastro@gmail.com>
2026-06-28 18:56:10 -0600

    change Acme Inc. to Alpha Boss Trader in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          Acme Inc.
+          Alpha Boss Trader
         </a>
         <LoginForm />
       </div>
